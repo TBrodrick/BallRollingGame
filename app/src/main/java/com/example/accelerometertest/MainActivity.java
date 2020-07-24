9829b4fde7118ca8c909b19ebadfecc5f5f06a32
TBrodrick <tristanbrodrick@gmail.com>
2020-07-23 20:16:26 -0700

    Demo

--- app/src/main/java/com/example/accelerometertest/MainActivity.java
+++ app/src/main/java/com/example/accelerometertest/MainActivity.java
@@ -9,30 +9,37 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity{
 
     private SensorManager myManager;
     private Sensor mAcc;
+    TextView valShow;
+    Toast t = Toast.makeText(this, "test", Toast.LENGTH_SHORT);
+
+    private SensorEventListener mAccEvenListener = new SensorEventListener() {
+        @Override
+        public final void onSensorChanged(SensorEvent event) {
+            valShow.setText(event.toString());
+            t.show();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        valShow = (TextView) findViewById(R.id.val);
+        valShow.setText("Test");
         myManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        mAcc = myManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    }
-
-    @Override
-    public final void onSensorChanged(SensorEvent event){
-        TextView valShow = (TextView)findViewById(R.id.val);
-        valShow.setText((int) event.values[0]);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        mAcc = myManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
     }
 }
