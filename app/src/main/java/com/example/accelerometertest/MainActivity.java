95d93d3aa3f55b76e8ba2dcfa84bb62c853b954f
TBrodrick <tristanbrodrick@gmail.com>
2020-08-07 19:51:07 -0700

    Collision right working. Holes not working. Collision left not working.

--- app/src/main/java/com/example/accelerometertest/MainActivity.java
+++ app/src/main/java/com/example/accelerometertest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -27,9 +28,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor myAcc;
     private ImageView gameBall;
     private ImageView[] Walls;
+    private Rect[] Holes;
     private int[] ballLocation = {0, 0};
     private float speedX;
     private float speedY;
+    private  TextView valshow;
+    private Toast testToast;
 
 
     @Override
@@ -44,13 +48,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         myAcc = myManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         myManager.registerListener(this, myAcc, SensorManager.SENSOR_DELAY_NORMAL);
 
+        valshow = findViewById(R.id.val);
+
         //setting up ball
         gameBall = (ImageView)findViewById(R.id.ball);
 
-        //setting up array of walls/individual walls
+        //setting up array of walls/individual walls and holes
+        Holes = new Rect[1];
+        ImageView Hole = (ImageView)findViewById(R.id.hole);
+        valshow.setText(Hole.toString());
+        Holes[0] = new Rect(Hole.getLeft(), Hole.getTop(), Hole.getRight(), Hole.getBottom());
         Walls = new ImageView[1];
-        ImageView wall = (ImageView)findViewById(R.id.imageView);
+        ImageView wall = (ImageView)findViewById(R.id.wall);
         Walls[0] = wall;
+        testToast = Toast.makeText(this, "test", Toast.LENGTH_SHORT);
     }
 
 
@@ -61,17 +72,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float speedMoveX = (float)(-571.4 * valX);
         float speedMoveY = (float)(571.4 * valY);
 
-        TextView valshow = (TextView)findViewById(R.id.val);
-        valshow.setText("speedMove: " + speedMoveY);
-
+        //Moving right (positive)
         if(speedMoveX > 0){
             for(int i = 0; i < Walls.length; i++){
+                if(Walls[i].getRight() >= (ballLocation[0] + gameBall.getWidth())
+                    && Walls[i].getLeft() < (ballLocation[0] + gameBall.getWidth())){
+                    if(((Walls[i].getTop() < (ballLocation[1] - gameBall.getHeight())) && Walls[i].getBottom() > ballLocation[1]) ||
+                            (ballLocation[1] < Walls[i].getBottom() && ballLocation[1] > Walls[i].getTop()) ||
+                            ((ballLocation[1] - gameBall.getHeight()) < Walls[i].getBottom() &&  (ballLocation[1] - gameBall.getHeight()) > Walls[i].getTop()))
+                        speedMoveX = 0;
 
+                }
             }
         }
+
+        //Moving left (negative)
         else{
             for(int i = 0; i < Walls.length; i++){
-
+                if(Walls[i].getRight() <= (ballLocation[0])
+                        && Walls[i].getLeft() > (ballLocation[0])) {
+                    if(((Walls[i].getTop() < (ballLocation[1] - gameBall.getHeight())) && Walls[i].getBottom() > ballLocation[1]) ||
+                            (ballLocation[1] < Walls[i].getBottom() && ballLocation[1] > Walls[i].getTop()) ||
+                            ((ballLocation[1] - gameBall.getHeight()) < Walls[i].getBottom() &&  (ballLocation[1] - gameBall.getHeight()) > Walls[i].getTop()))
+                        testToast.show();
+                }
             }
         }
 
@@ -79,8 +103,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //right or left edges of any of the walls. If it is, sets speedMoveY to zero.
         if(speedMoveY > 0){
             for(int i = 0; i < Walls.length; i++){
-                if(Walls[i].getTop() <= ((ballLocation[1] - (gameBall.getHeight()/2)) + 25)
-                        && Walls[0].getBottom() > ballLocation[1])
+                if(Walls[i].getTop() <= (ballLocation[1] - 75)
+                        && Walls[0].getBottom() > (ballLocation[1] - 75))
                {
                     if((Walls[i].getLeft() <= ((ballLocation[0] + gameBall.getWidth() - 25)))
                             && ((Walls[i].getRight() > ((ballLocation[0] - gameBall.getWidth() + 213)))))
@@ -92,8 +116,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //right or left edges of any of the walls. If it is, sets speedMoveY to zero.
         else{
             for(int i = 0; i < Walls.length; i++){
-                if(Walls[i].getBottom() >= (ballLocation[1] - (gameBall.getHeight()) - 50)
-                    && Walls[0].getTop() < ballLocation[1]) {
+                if(Walls[i].getBottom() >= (ballLocation[1] - (gameBall.getHeight()*1.5))
+                    && Walls[i].getTop() < (ballLocation[1] - (gameBall.getHeight()*1.5))) {
                     if((Walls[i].getLeft() <= ((ballLocation[0] + gameBall.getWidth())))
                             && ((Walls[i].getRight() > ((ballLocation[0] - gameBall.getWidth()) + 213))))
                         speedMoveY = 500;
@@ -102,18 +126,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }
 
-            //sets up animation of ball in X direction
-            ObjectAnimator animationX = ObjectAnimator.ofFloat(gameBall, "x", speedMoveX);
-            animationX.setDuration(4000);
+        //sets up animation of ball in X direction
+        ObjectAnimator animationX = ObjectAnimator.ofFloat(gameBall, "x", speedMoveX);
+        animationX.setDuration(4000);
 
-            //sets up animation of ball in X direction
-            ObjectAnimator animationY = ObjectAnimator.ofFloat(gameBall, "y", speedMoveY);
-            animationY.setDuration(4000);
+        //sets up animation of ball in X direction
+        ObjectAnimator animationY = ObjectAnimator.ofFloat(gameBall, "y", speedMoveY);
+        animationY.setDuration(4000);
 
-            //plays X and Y animation of ball
-            AnimatorSet animator = new AnimatorSet();
-            animator.playTogether(animationX, animationY);
-            animator.start();
+        //plays X and Y animation of ball
+        AnimatorSet animator = new AnimatorSet();
+        animator.playTogether(animationX, animationY);
+        animator.start();
+
+        //Define ball rect
+        Rect BallRect = new Rect(ballLocation[0], ballLocation[1], (ballLocation[0] + gameBall.getWidth()), (ballLocation[1] - gameBall.getHeight()));
+
+        //valshow.setText(Holes[0].toString());
+        //Check if the ball fell in a hole
+        for(int i = 0; i < Holes.length; i++){
+            if(Holes[i].intersect(BallRect)) {
+                testToast.show();
+                gameBall.setX(0);
+                gameBall.setY(0);
+            }
+        }
 
         //gets the location of the ball image on the screen and stores it in the ballLocation variable
         gameBall.getLocationOnScreen(ballLocation);
